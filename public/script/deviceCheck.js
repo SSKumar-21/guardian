01d45693d4c6d7bd0e1e7c41d1a0d521705216e3
SIDHARTH KUMAR RATHORE <141614109+SSKumar-21@users.noreply.github.com>
2025-08-07 18:52:34 +0530

    Delete public directory

--- public/script/deviceCheck.js
+++
@@ -1,7 +0,0 @@
-window.onload = function () {
-   
-    if (!/iPhone|Android/i.test(navigator.userAgent)) {
-        alert("This website is only accessible on smartphones.");
-        document.body.innerHTML = ""; // Clear the page content
-    }
-};
